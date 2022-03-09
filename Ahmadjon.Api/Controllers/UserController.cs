495a32034c9428155b8a8a44f6b53f81025b0b0a
Mukhammadkarim Tukhtaboyev <75162831+MrTukhtaboyev@users.noreply.github.com>
2022-03-09 11:36:47 +0500

    asdas

---
+++ Ahmadjon.Api/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Ahmadjon.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoLibrary;
+
+namespace Ahmadjon.Api.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<RedirectResult> DownloadYoutubeVideo([FromForm]VideoModel model)
+        {
+            YouTube youtube = YouTube.Default;
+            YouTubeVideo video = await youtube.GetVideoAsync(model.Url);
+            string videoUrl = await video.GetUriAsync();
+
+            return Redirect(videoUrl);
+        }
+    }
+}
---
+++ Ahmadjon.Api/Models/VideoModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Ahmadjon.Api.Models
+{
+    public class VideoModel
+    {
+        public string Url { get; set; }
+        public int Resolution { get; set; }
+    }
+}
